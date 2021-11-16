19c7e2602887bb062430da9a090a464fc3c5fc8b
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-16 13:29:10 -0700

    edit_meal HTML Setup

--- flask_app/controllers/controller_users.py
+++ flask_app/controllers/controller_users.py
@@ -9,4 +9,16 @@ bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
+    return render_template('dashboard.html')
+
+@app.route('/meals')
+def meals():
+    return render_template('my_meals.html')
+
+@app.route('/edit_meal')
+def edit():
+    return render_template('edit_meal.html')
+
+@app.route('/add_meal')
+def add():
     return render_template('add_meal.html')
