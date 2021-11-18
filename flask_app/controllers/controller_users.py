abeaab65bea0b65e35bb99430e176353697acb89
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-18 09:55:02 -0700

    Login and Guest  Login Updated

--- flask_app/controllers/controller_users.py
+++ flask_app/controllers/controller_users.py
@@ -7,9 +7,71 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
 
+
+
+#  First Stop Index Page
 @app.route('/')
 def index():
     return render_template('login.html')
+
+#  From GUEST PAGE to LOGIN
+@app.route('/guest_login')
+def guest_login():
+    return render_template('guest_login.html')
+
+#   from FORM to LOGIN USER
+@app.route('/login', methods = ['POST'])
+def user_login():
+    if not model_user.User.validate_user_login(request.form):
+        return redirect('/')
+
+    data = { 
+        "email" : request.form["email"] 
+        }
+    user_in_db = model_user.User.get_user_by_email(data)
+    if not user_in_db:
+        flash("Invalid Email/Password", category= "user_email")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("Invalid Email/Password", category= "user_email")
+        return redirect('/')
+
+    session['user_id'] = user_in_db.id
+    print(session['user_id'])
+    return redirect('/dashboard')
+
+
+
+#   Route To REGISTRATION page
+@app.route('/registration')
+def user_registration():
+    return render_template('user_registration.html')
+    
+#   from FORM to REGISTER NEW USER
+@app.route('/register', methods= ['POST'])
+def register():
+    if not model_user.User.validate_user_registration(request.form):
+        return redirect('/registration')
+    
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email'],
+        'password' : pw_hash
+    }
+    user_id = model_user.User.create_user(data)
+    session['user_id'] = user_id
+
+    return redirect('/dashboard')
+
+
+#  Route FROM Login/Registration TO DASHBOARD
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
 
 @app.route('/meals')
 def meals():
@@ -22,3 +84,10 @@ def edit():
 @app.route('/add_meal')
 def add():
     return render_template('add_meal.html')
+
+
+#  ROUTE For USER to LOGOUT
+@app.route('/logout')
+def user_logout():
+    session.clear()
+    return redirect('/')
--- flask_app/models/model_user.py
+++ flask_app/models/model_user.py
@@ -7,31 +7,73 @@ import re
 from flask_bcrypt import Bcrypt        
 bcrypt = Bcrypt(app) 
 
-
-
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+model_db = "meals"
 
 
 class User:
     def __init__(self,data):
         self.id = data['id']
-
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
 
+#   To VALIDATE USER REGISTRATION Info
+    @staticmethod
+    def validate_user_registration(user):
+        is_valid = True
+        if len(user['first_name']) < 1: 
+            flash("A First Name must be included.", category= "reg_first_name")
+            is_valid = False  
+        if len(user['last_name']) < 1: 
+            flash("A Last Name must be included.", category= "reg_last_name")
+            is_valid = False  
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Please enter a valid Email.", category= "reg_email")
+            is_valid = False
+        if len(user['password']) < 1: 
+            flash("A Password must be included.", category= "reg_password")
+            is_valid = False  
+        if user['password'] != user['confirm_password']: 
+            flash("Passwords do not match.", category= "reg_confirm_password")
+            is_valid = False  
+        return is_valid
 
-
+#   To VALIDATE USER LOGIN Credentials
+    @staticmethod
+    def validate_user_login(user):
+        is_valid = True
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Please enter a valid Email.", category= "login_email")
+            is_valid = False
+        if len(user['password']) < 1: 
+            flash("Please enter a password.", category= "login_password")
+            is_valid = False  
+        return is_valid
 
     # ***CREATE***
 
-
+    @classmethod
+    def create_user(cls,data):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+        return connectToMySQL(model_db).query_db(query, data)
 
 
 
 
     # ***Retreive***
 
-
+    @classmethod
+    def get_user_by_email(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(model_db).query_db(query,data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
 
 
 
